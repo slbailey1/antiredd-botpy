be4464356e99fa3ea93a2acc9c7364b2fd574169
Sam <49822575+slbailey1@users.noreply.github.com>
2020-05-10 22:33:03 -0400

    Update ban_reddit.py

--- ban_reddit.py
+++ ban_reddit.py
@@ -1,5 +1,13 @@
+# DOWN WITH REDDIT
+# a simple discord bot
+# that filters everything to do
+# w i t h  r e d d i t 
+
 import discord
 client = discord.Client()
+
+list = [
+'reddit', 'chungus', 'redddit', 'readit', 'rediit', 'chunguus', 'chhungus', 'chunggus', 'chungggus', 'reddi t','r e d d i t', 'chungūs', 'red dit', 'red it', 'red dirt', 'roadit', 'redd it'] 
 
 @client.event
 async def on_ready():
@@ -10,9 +18,10 @@ async def on_message(message):
     msglower = message.content.lower()
     print(message.author,':', message.content)
     if 'reddit.com' in msglower or 'redd.it' in msglower:
-    	return
-    elif 'reddit' in msglower or 'chungus' in msglower:
+    	print('CLOSE CALL')
+    elif msglower in list:
     	await message.delete()
     	print('REDDIT DETECTED')
+    	
 
-client.run('token')
+client.run('token-here')
