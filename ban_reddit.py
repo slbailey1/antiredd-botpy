ca7b05ded74199e6ee82cea8b453d63594d1fad3
iHatereddit <65142028+iHatereddit@users.noreply.github.com>
2020-05-11 15:27:59 -0400

    Add files via upload

--- ban_reddit.py
+++ ban_reddit.py
@@ -1,8 +1,10 @@
-
 import discord
+from difflib import SequenceMatcher, get_close_matches
+
 client = discord.Client()
 
-from iteration import *
+hate = 'reddit'
+hate2 = 'chungus'
 
 @client.event
 async def on_ready():
@@ -10,13 +12,24 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+
+    
     msglower = message.content.lower()
+    seq = SequenceMatcher(a=hate, b=msglower)
+    seq2 = SequenceMatcher(a=hate2, b=msglower)
+    
+    print(seq.ratio())
+    print(seq2.ratio())
     print(message.author,":", message.content)
     if 'reddit.com' in msglower or 'redd.it' in msglower:
     	print("CLOSE CALL")
-    elif msglower or message.content() in list:
+    	return
+    if seq.ratio() >= 0.7000000:
     	await message.delete()
     	print("REDDIT DETECTED")
-    	
+    if seq2.ratio() >= 0.7000000:
+            await message.delete()
+            print("REDDIT DETECTED")
 
-client.run('Token here')
+
+client.run('token-here')
