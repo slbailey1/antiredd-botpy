beb8aeaf6115ab250729a680fe65574ca6895cfa
iHatereddit <65142028+iHatereddit@users.noreply.github.com>
2020-05-11 15:26:01 -0400

    Delete iteration.py

--- iteration.py
+++
@@ -1,4 +0,0 @@
-class string:
-    def __init__(self):
-        list = ['reddit', 'chungus', 'redddit', 'readit', 'rediit', 'chunguus', 'chhungus', 'chunggus', 'chungggus', 'reddi t','r e d d i t', 'chungūs', 'red dit', 'red it', 'red dirt', 'roadit', 'redd it']
-   
