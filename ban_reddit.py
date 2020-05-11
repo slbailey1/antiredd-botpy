0131bfdbf73b4a0cc27ccf5a3c31e46e977b3b78
iHatereddit <65142028+iHatereddit@users.noreply.github.com>
2020-05-11 13:13:56 -0400

    Add files via upload
    Now deletes entire messages with multiple iterations of reddit and chungus, and added a separate list file

--- ban_reddit.py
+++ ban_reddit.py
@@ -1,27 +1,22 @@
-# DOWN WITH REDDIT
-# a simple discord bot
-# that filters everything to do
-# w i t h  r e d d i t 
 
 import discord
 client = discord.Client()
 
-list = [
-'reddit', 'chungus', 'redddit', 'readit', 'rediit', 'chunguus', 'chhungus', 'chunggus', 'chungggus', 'reddi t','r e d d i t', 'chungūs', 'red dit', 'red it', 'red dirt', 'roadit', 'redd it'] 
+from iteration import *
 
 @client.event
 async def on_ready():
-    print('Working.')
+    print("Working.")
 
 @client.event
 async def on_message(message):
     msglower = message.content.lower()
-    print(message.author,':', message.content)
+    print(message.author,":", message.content)
     if 'reddit.com' in msglower or 'redd.it' in msglower:
-    	print('CLOSE CALL')
-    elif msglower in list:
+    	print("CLOSE CALL")
+    elif msglower or message.content() in list:
     	await message.delete()
-    	print('REDDIT DETECTED')
+    	print("REDDIT DETECTED")
     	
 
-client.run('token-here')
+client.run('Token here')
